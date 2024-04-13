f1c258d2ca6b8450c883146c5bdd7f35442c5714
pirotyyy <hiroki1423@docomo.ne.jp>
2024-04-13 20:08:44 +0900

    💄 css

--- src/content/Content.tsx
+++ src/content/Content.tsx
@@ -2,7 +2,7 @@ import LgtmModal from '../app/features/LgtmModal/LgtmModal';
 
 const Content = () => {
   return (
-    <div className="absolute z-[999] top-1/2 right-1/2 shadow-xl border-[1px] bg-white bg-opacity-10">
+    <div className="fixed z-[999] top-1/2 right-1/2 shadow-xl">
       <LgtmModal />
     </div>
   );
