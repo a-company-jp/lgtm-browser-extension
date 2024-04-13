937dcef5ec1554b6d66f255e708de2543ddd583e
pirotyyy <hiroki1423@docomo.ne.jp>
2024-04-13 20:05:03 +0900

    ✨ 初期設定

---
+++ src/app/components/LgtmList.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LgtmList = () => {
+  return <div>LgtmList</div>;
+};
+
+export default LgtmList;
---
+++ src/app/features/LgtmModal/LgtmModal.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LgtmModal = () => {
+  return (
+    <>
+      <div className="p-4 bg-blue-700 outline rounded">Lgtm Modal</div>
+    </>
+  );
+};
+
+export default LgtmModal;
