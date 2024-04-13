409821bbf9bb7511fde7eee046e599866dffe943
pirotyyy <hiroki1423@docomo.ne.jp>
2024-04-14 00:56:34 +0900

    ✨ PopupのUI作成

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,9 +1,31 @@
+import { useState } from 'react';
+import LgtmList from '../app/components/LgtmList';
+import GenerateLgtmModal from '../app/components/GenerateLgtmModal';
+
 const Popup = () => {
   document.body.className = 'w-[30rem] h-[15rem]';
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
 
   return (
     <>
-      <div className="flex justify-center mt-2 text-base">LGTM Generator</div>
+      <div className="p-4">
+        <div className="flex justify-between">
+          <p className="text-2xl font-bold">LGTM Generator</p>
+          <button onClick={openModal} className="p-2 text-sm outline rounded hover:bg-gray-100">
+            + Generate
+          </button>
+        </div>
+        <hr className="my-4" />
+        <div>
+          <LgtmList />
+        </div>
+      </div>
+
+      {showModal && <GenerateLgtmModal setShowModal={setShowModal} />}
     </>
   );
 };
